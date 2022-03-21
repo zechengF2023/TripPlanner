eeff9e2fcb587dfd4372cf6547a4a08aa698edaa
ave235 <andrey.emme@gmail.com>
2022-03-20 21:00:51 -0400

    uploading profile js and css, co-author with ailaamir

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,13 +1,35 @@
-import React from "react"
+import React, {useMemo} from "react"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import savedTrips from "../components/savedTripsData.json"
+import RecommendationDestination from "../components/RecommendationDestination"
+import "../css/Profile.css"
 
-const Profile=props=>
-    (
+
+const Profile=()=> {
+    const trips = useMemo(() => {
+        if (!savedTrips){
+            return [];
+        } else {
+            return savedTrips;
+        }
+    }, []);
+
+    return (
         <div>
             <Header/>
-            <p>Here is profile</p>
-            <Footer />
+            <div className="secondaryContainer">
+                <p>Welcome, [placeholder]!</p>
+                <a href="/settings" className="linkToSettings">Settings</a>
+            </div>
+            <h2 className="subtitle">Your saved trips:</h2>
+            <div className="tripHolder">
+                {trips.map((destination, i) =>
+                    <RecommendationDestination destination={destination} key={i}/>
+                )}
+            </div>
+            <Footer/>
         </div>
     )
+}
 export default Profile
