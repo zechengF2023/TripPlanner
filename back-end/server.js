7c72062e355d29ba3c022bfeddaa38fa51cd5ee6
ZCGuoo <zg922@nyu.edu>
2022-03-23 12:02:36 -0400

    backend setup included

---
+++ back-end/server.js
@@ -0,0 +1,11 @@
+const server = require("./app")
+const port = 3000 
+const listener = server.listen(port, function () {
+  console.log(`Server running on port: ${port}`)
+})
+const close = () => {
+  listener.close()
+}
+module.exports = {
+  close: close,
+}
