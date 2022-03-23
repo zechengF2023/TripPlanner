a2426ca8226157e909db0dbe450c1c0c2e7b2edf
JulesKho <jewels.co18@gmail.com>
2022-03-23 16:54:19 -0400

    data loading issue fixed

--- front-end/src/pages/SingleResult.js
+++ front-end/src/pages/SingleResult.js
@@ -31,71 +31,70 @@ function SingleResult(){
         }
     }, [article])
 
-    console.log(article); 
-    console.log(articleImages); 
-    console.log(articleActivities); 
-
-    return (
-        <div>
-            <Header/>
-            <div style = {{backgroundColor: "white"}}>
-                <div style={{ marginLeft: "100px", marginRight: "100px", height: "400px",backgroundColor: "white"}}>
-                    <Carousel>
-                        {articleImages.map( (item, i) => <CarouselItem key={i} item={item} /> )}
-                    </Carousel>
-                </div>
-
-                <div className="singleContent" style={{marginTop: "30px", marginLeft:"100px", marginRight:"100px"}}>
-                    <div className="singleContentLeft">
-                        <h2 className="singleName">{article.name}</h2>
-                        <h4 className="singleLocation">{article.location}</h4>
-                        <p> {article.amenities} </p>
-                        <h4 className="singleTagline">{article.tagline}</h4>
-                        <p className="singleBlurb">{article.blurb}</p>
-
-                        <Button>
-                            Book a Room
-                        </Button>
-                    </div>
-                    <div className="singleContentRight">
-                        {articleActivities.map( (item, i) => <ActivitiesItem key={i} item={item} /> )}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function CarouselItem(props)
-{
-    return (
-        <Paper style={{height: "400px"}}>
+    if (article === {} || articleImages === undefined || articleActivities === undefined) {
+        return (<> <h1>Still Loading...</h1></>);
+    }
+    else{
+        return (
             <div>
-                {/* <img alt="hotelPhotos" src={props.item.photo}/> */}
-                <img alt="hotelPhotos" src={hotelPhoto}/>
-            </div>  
-        </Paper>
-    )
-}
-
-function ActivitiesItem(props)
-{
-    return (
-        <div className="singleActivityContent" style ={{height:"100px"}}>
-            <div className="singleActivityLeft">
-                {/* <img alt="activityPhoto" src={props.item.image}/> */}
-                <img alt="activityPhoto" src={activityPhoto} style={{width: "100%"}}/>
+                <Header/>
+                <div style = {{backgroundColor: "white"}}>
+                    <div style={{ marginLeft: "100px", marginRight: "100px", height: "400px",backgroundColor: "white"}}>
+                        <Carousel>
+                            {articleImages.map( (item, i) => <CarouselItem key={i} item={item} /> )}
+                        </Carousel>
+                    </div>
+    
+                    <div className="singleContent" style={{marginTop: "30px", marginLeft:"100px", marginRight:"100px"}}>
+                        <div className="singleContentLeft">
+                            <h2 className="singleName">{article.name}</h2>
+                            <h4 className="singleLocation">{article.location}</h4>
+                            <p> {article.amenities} </p>
+                            <h4 className="singleTagline">{article.tagline}</h4>
+                            <p className="singleBlurb">{article.blurb}</p>
+    
+                            <Button> Book a Room </Button>
+                        </div>
+                        <div className="singleContentRight">
+                            {articleActivities.map( (item, i) => <ActivitiesItem key={i} item={item} /> )}
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="singleActivityRight">
-                <h3 style={{margin:"0px"}}>{props.item.name}</h3>
-                <h5 style={{margin:"0px"}}>{props.item.price}</h5>
-                <Button>
-                    Add Activity
-                </Button>
+        )
+    }
+        
+    function CarouselItem(props)
+    {
+        return (
+            <Paper style={{height: "400px"}}>
+                <div>
+                    {/* <img alt="hotelPhotos" src={props.item.photo}/> */}
+                    <img alt="hotelPhotos" src={hotelPhoto}/>
+                </div>  
+            </Paper>
+        )
+    }
+        
+    function ActivitiesItem(props)
+    {
+        return (
+            <div className="singleActivityContent" style ={{height:"100px"}}>
+                <div className="singleActivityLeft">
+                    {/* <img alt="activityPhoto" src={props.item.image} style={{width:"100%"}}/> */}
+                    <img alt="activityPhoto" src={activityPhoto} style={{width: "100%"}}/>
+                </div>
+                <div className="singleActivityRight">
+                    <h3 style={{margin:"0px"}}>{props.item.name}</h3>
+                    <h5 style={{margin:"0px"}}>{props.item.price}</h5>
+                    <Button>
+                        Add Activity
+                    </Button>
+                </div>
+    
             </div>
-
-        </div>
-    )
+        )
+    }   
 }
 
 export default SingleResult
