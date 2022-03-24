40e702b32d1f854592702ef7b5085721f2bcc2db
ZCGuoo <zg922@nyu.edu>
2022-03-23 21:09:18 -0400

    contact page backend finished

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,17 @@
 const express = require("express") 
 const app = express() 
+const cors=require("cors")
+const bodyParser=require("body-parser")
+app.use(bodyParser.json())
+app.use(cors())
+
+
+/* data from contact page*/
+app.post("/contact",(req,res)=>{
+    const contactData=req.body
+    res.sendStatus(200).end()
+    /*store in database*/
+})
+
 
 module.exports = app
