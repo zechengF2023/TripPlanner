1478ef304991f38ca10e445952d66ca73207c601
ZCGuoo <zg922@nyu.edu>
2022-04-07 13:26:05 -0400

    about page finished

--- front-end/src/pages/About.js
+++ front-end/src/pages/About.js
@@ -1,13 +1,23 @@
+import "../css/About.css"
 import React from "react"
 import Footer from "../components/Footer.js"
 import Header from "../components/Header"
-
+import logo from "../assets/Logo.png"
 const About=()=>{
     return(
         <div>
             <Header />
-            <p>This is About!</p>
-            <Footer />
+                <div className='content'>
+                    <h1>About Us</h1>
+                    <p>The inspiration of this project arises from the fact that it's often time-consuming for people to plan trips themselves. To make an interesting and memorable trip, one would have to do vast research on local hotels, tourist attractions , restaurants, etc. With the goal of exempting you from such trouble, the Trip Planner app aims to generate trips based on your preferences, saving your time while catering to your needs. </p>
+                    <p>
+                        Wish you a pleasant user experience! 
+                    </p>
+                    <img src={logo} id="logoImg"></img>
+                <div className="footerDiv">
+                <Footer />
+                </div>
+            </div>
         </div>
     )
 }
