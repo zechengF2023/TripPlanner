7311a8d8cd8a7deeacdeb4496211fc98bffc8032
Celina Hu <ch3407@nyu.edu>
2022-04-08 12:47:07 -0400

    backend for login and signup pages

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,49 @@
+require('dotenv').config({ silent: true })
 const express = require("express") 
 const app = express() 
 const cors=require("cors")
+const morgan = require("morgan")
 const bodyParser=require("body-parser")
 app.use(bodyParser.json())
+app.use(morgan('dev', {skip: (req, res) => process.env.NODE_ENV === 'test'}))
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const fs = require('fs')
+const users = require("./data/user.json")
+const trips = require("./data/trip.json")
+
+// signup page
+app.post("/signup", (req, res) => {
+    const userData = {username: req.body.name, password: req.body.password}
+    console.log(userData)
+    users.push(userData)
+    fs.writeFile("./data/user.json", JSON.stringify(users), (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("File written successfully\n");
+        }
+    })
+    res.json(users)
+})
+
+// login page
+app.post("/login", (req, res) => {
+    let success = 0
+    const userData = {username: req.body.name, password: req.body.password}
+    console.log(userData)
+    users.map(user => {
+        if(user.username === userData.username && user.password === userData.password) {success=1}
+    })
+    if (success === 1) {
+        res.json(true)
+    }
+    else {
+        res.json(false)
+    }
+})
 
 
 /* data from contact page*/
--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,10 +1,14 @@
-import TextField from '@mui/material/TextField';
+//import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Logo from '../assets/Logo.png';
 import '../css/Login.css';
 import {Link} from "react-router-dom"
+import React, {useEffect, useState} from "react";
+import {Navigate} from "react-router-dom";
 
+const axios = require("axios")
 
+/*
 const Login=()=> {
 
     // function logIn(e) {
@@ -16,7 +20,7 @@ const Login=()=> {
        <div className="loginContainer">
           <div className="card">
              <div className='logoContainer'>
-                <img src={Logo}/>
+                <img src={Logo} alt="logo"/>
              </div>
               <h1 className='heading'>Welcome back Traveler!</h1>
               <div className="form-row">
@@ -35,6 +39,65 @@ const Login=()=> {
           </div>
        </div>
     )
+} */
+
+
+function Login() {
+    const [loggedin, setLoggedin] = useState(false)
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+  
+    useEffect(() => {
+        setLoggedin(false)
+        console.log(loggedin)
+    }, [loggedin])
+  
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(loggedin)
+        axios
+            .post(`${process.env.REACT_APP_SERVER_HOSTNAME}/login`, {name: name, password: password})
+            .then((response) => {setLoggedin(response.data)})
+            .catch(err => {console.log(`error! ${err}`)})
+            setName('')
+            setPassword('')
+      
+    }
+    
+    const renderForm = (
+        <div className="loginContainer">
+            <div className="card">
+                <form onSubmit={handleSubmit}>
+                    <div className='logoContainer'>
+                        <img src={Logo} alt="logo"/>
+                    </div>
+                    <h1 className='heading'>Welcome back Traveler!</h1>
+                    <div className="form-row">
+                        <label>User Name</label>
+                        <input type="text" value={name} required onChange={e => setName(e.target.value)} />
+                    </div>
+                    <div className="form-row">
+                        <label>Password</label>
+                        <input type="password" value={password} required onChange={e => setPassword(e.target.value)} />
+                    </div>
+                    <Button onClick="logIn()" style={{width:"100%", background: "grey"}} size="large" variant="contained" href="/">Login</Button>
+                    <div>
+                        <h3>Not yet a member?</h3>
+                        <h3><Link to="/signup">Create an Account</Link>  here.</h3>
+                    </div>
+                </form>
+            </div>
+       </div>
+    )
+    if (true) {
+        return (
+            <div className="loginContainer">
+                <div className="form-row">
+                    {loggedin ?<Navigate to = "/"/>: renderForm}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Login
